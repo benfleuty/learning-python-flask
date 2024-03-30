c2cb3dc1a25b0252096d3e1195ce66eacd47a5e3
Ben Fleuty <bfleuty@outlook.com>
2024-03-30 23:41:41 +0000

    Add template with posts

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Ben'}
-    return '''
-    <html><head><title>Home Page - Microblog</title></head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-    </html>'''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Dundee!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers move was so so.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
