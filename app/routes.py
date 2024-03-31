49866dfde0b7856a0bc2f986a44a49ff145c5738
Ben Fleuty <bfleuty@outlook.com>
2024-03-31 15:54:57 +0100

    Add register capability

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from urllib.parse import urlsplit
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 import sqlalchemy as sa
 from app import db
@@ -18,6 +18,21 @@ def index():
         {"author": {"username": "Susan"}, "body": "The Avengers move was so so."},
     ]
     return render_template("index.html", title="Home", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
